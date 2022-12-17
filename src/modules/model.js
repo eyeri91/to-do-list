18da43a7ffade17865138d80a75f7c7c24b3ef2f
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-12-17 20:12:48 +0400

    Merge

---
+++ src/modules/model.js
@@ -0,0 +1,32 @@
+<<<<<<< Updated upstream
+=======
+import Task from "./task.js";
+import Project from "./project.js";
+
+const newTask = new Task("Pack");
+console.log(newTask);
+
+const personal = new Project("Personal");
+personal.addTask(newTask);
+console.log(personal.getAllTasks());
+
+class toDoList {
+  constructor() {}
+}
+
+// A function to return all tasks regardless of projects
+// A function to return all tasks that is due for today
+// A function to return all tasks that is due for this week
+// A function to return all tasks that is important
+
+// A function to save tasks to localStorage
+
+const returnTasks =
+  (...locationToCheck) =>
+  (conditionsToCheck) => {
+    // return tasks that meets conditions to check
+  };
+
+// const returnTodayTask = returnTasks(personal);
+// const todayTaskFromPersonal = returnTodayTask(date);
+>>>>>>> Stashed changes
---
+++ src/modules/view.js
@@ -0,0 +1,31 @@
+export class View {
+  constructor(taskListContainer, sideBar) {
+    this.taskListContainer = taskListContainer;
+    this.sideBar = sideBar;
+  }
+
+  // Function to update taskListContainer with chose list
+  // Function to add and remove project to the sideBar
+
+  renderStartPage() {
+    // renderPage
+  }
+
+  renderProjectList() {}
+
+  addNewProject() {}
+
+  removeProject() {}
+
+  addTask() {}
+
+  removeTask() {
+    //
+  }
+
+  editTaskDetails() {}
+
+  togglePriority() {}
+
+  updateProjectCategory() {}
+}
