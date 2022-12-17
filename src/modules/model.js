ec4d807945446d6ff4263f78140da9d3e023cf3a
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-12-17 20:15:38 +0400

    Import Task andn Project class

--- src/modules/model.js
+++ src/modules/model.js
@@ -7,3 +7,23 @@ console.log(newTask);
 const personal = new Project("Personal");
 personal.addTask(newTask);
 console.log(personal.getAllTasks());
+
+class toDoList {
+  constructor() {}
+}
+
+// A function to return all tasks regardless of projects
+// A function to return all tasks that is due for today
+// A function to return all tasks that is due for this week
+// A function to return all tasks that is important
+
+// A function to save tasks to localStorage
+
+const returnTasks =
+  (...locationToCheck) =>
+  (conditionsToCheck) => {
+    // return tasks that meets conditions to check
+  };
+
+// const returnTodayTask = returnTasks(personal);
+// const todayTaskFromPersonal = returnTodayTask(date);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,9 @@
 export function toSentenceCase(string) {
   return string[0].toUpperCase() + string[0].slice(1);
 }
+
+export function createElement(elementType, elementText) {
+  const element = createElement(elementType);
+  if (elementText) element.textCotent = elementText;
+  return element;
+}
