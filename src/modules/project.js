0806c7a06b004dc50c67a85dd36da4146f9415ff
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-12-17 14:17:40 +0400

    Add basic functions for Project class

---
+++ src/modules/project.js
@@ -0,0 +1,41 @@
+export default class Project {
+  #tasks;
+  constructor(title) {
+    this.title = title;
+    this.#tasks = [];
+  }
+
+  addTask(newTask) {
+    this.#tasks.push(newTask);
+  }
+
+  deleteTask(taskToBeDeleted) {
+    this.#tasks = this.#tasks.filter(
+      (task) => task.getTitle() !== taskToBeDeleted.getTitle()
+    );
+  }
+
+  getTask(taskToBeFound) {
+    const foundTask = this.#tasks.find(
+      (task) => task.getTitle() === taskToBeFound.getTtitle()
+    );
+  }
+
+  getAllTasks() {
+    if (this.#tasks) {
+      return this.#tasks;
+    }
+  }
+
+  getTasksForThisWeek() {
+    // Get tasks that is due for this week
+  }
+
+  getTasksForToday() {
+    // Get tasks that is due for today
+  }
+
+  getImportantTasks() {
+    // get tasks that has priority is set true
+  }
+}
