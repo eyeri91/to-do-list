fe13563dc24d22962efb341a7a60760ba736f9d2
Yuri The Doer <70323981+eyeri91@users.noreply.github.com>
2022-12-24 14:54:01 +0400

    Change flex direction of navbar depending on size of screens

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   plugins: [new miniCssExtractPlugin()],
+
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
